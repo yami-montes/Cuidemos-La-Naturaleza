c4eb2c5f1ff92816d1d37068c706f62785aff2a0
yami-montes <yamilemontescardona808@gmail.com>
2025-10-06 15:13:46 -0500

    fix: rutas correctas para imágenes en GitHub Pages

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ function Gallery() {
     <section id="galeria">
       <h2>Galería de Naturaleza</h2>
       <div className="galeria">
-        <img src="/imagenes/bosques.jpg" alt="Bosque verde" />
+        <img src="/Cuidemos-La-Naturaleza/imagenes/bosques.jpg" alt="Bosques" />
         <img src="/imagenes/flores.jpg" alt="Flores naturales" />
         <img src="/imagenes/vacas.jpg" alt="Animales en su hábitat" />
       </div>
